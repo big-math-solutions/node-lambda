e94ce3ef4d5af493dec1b9f9fae516dcd84eb236
cereceres <jesus.edelcereceres@gmail.com>
2018-02-01 21:56:15 -0600

    logs are removed

--- bin/invoke-handler.js
+++ bin/invoke-handler.js
@@ -6,14 +6,10 @@ const readFile = promisify(fs.readFile);
 
 module.exports = (invokeName, payload, payloadFrom, logtype) => Promise.resolve()
     .then(() => {
-        console.log('payloadFrom ', payloadFrom);
         if (!payload && payloadFrom) return readFile(payloadFrom);
         return payload || '';
     })
     .then((data) => data.toString())
-    .then((data) => {
-        console.log('data ', data);
-        return handlers.invoke(invokeName, parseData(data), logtype);
-    })
+    .then((data) => handlers.invoke(invokeName, parseData(data), logtype))
     .then((res) => console.info(`response: ${JSON.stringify(res)}`))
     .catch((error) => console.error(`${error.message}`));
